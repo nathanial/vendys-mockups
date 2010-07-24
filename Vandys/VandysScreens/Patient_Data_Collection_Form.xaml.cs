fc721b39d3e418cb939602e620dc79015451b740
nathan <nathanial.hartman@gmail.com>
2010-07-23 23:55:09 -0500

    first commit

---
+++ Vandys/VandysScreens/Patient_Data_Collection_Form.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VandysScreens
+{
+	/// <summary>
+	/// Interaction logic for Patient_Data_Collection_Form.xaml
+	/// </summary>
+	public partial class Patient_Data_Collection_Form : UserControl
+	{
+		public Patient_Data_Collection_Form()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
---
+++ Vandys/VandysScreens/Retried_Old_Test_Results_Form.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VandysScreens
+{
+	/// <summary>
+	/// Interaction logic for Retried_Old_Test_Results_Form.xaml
+	/// </summary>
+	public partial class Retried_Old_Test_Results_Form : UserControl
+	{
+		public Retried_Old_Test_Results_Form()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
